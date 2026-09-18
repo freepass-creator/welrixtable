3af9ff14514b726c8cc5f7330fbc1536e56fb6de
freepass-creator <dudguq@gmail.com>
2026-09-18 20:58:58 +0900

    기간 선택과 발송 체크 상태를 같은 개수로 유지

--- src/components/mobile/StepConditions.vue
+++ src/components/mobile/StepConditions.vue
@@ -26,6 +26,10 @@ function toggleTerm(t) {
     list.push({ term: t, dep: quoteState.cond.dep || 10, pre: quoteState.cond.pre || 0 });
     list.sort((a, b) => b.term - a.term);
   }
+  /* 발송 체크도 현재 시나리오 개수와 같은 길이를 유지한다. */
+  if (!Array.isArray(quoteState.send)) quoteState.send = [];
+  while (quoteState.send.length < list.length) quoteState.send.push(true);
+  if (quoteState.send.length > list.length) quoteState.send.splice(list.length);
 }
 
 </script>
